8511a3b4e2853961d117bafcf53df8ad18a7c40a
Your Name <you@example.com>
2025-12-10 22:24:57 +0200

    add function check if game over

--- game/player.js
+++ game/player.js
@@ -8,19 +8,74 @@ export function createPlayer(sign) {
     }
 }
 
-export function createPlayers(){
+export function createPlayers() {
     const signA = readLineSync.question("what sign player A: ")
     const playerA = createPlayer(signA.toUpperCase())
-    const signs = ["X","O"]
+    const signs = ["X", "O"]
     let signB
-    for (let s of signs){
+    for (let s of signs) {
         signB = s
-        if (signB != signA){
+        if (signB != signA) {
             break
         }
     }
     console.log("now player B");
     const playerB = createPlayer(signB)
-    return [playerA,playerB]
+    return [playerA, playerB]
+}
+
+export function checkIsOver(board, players) {
+    let sign
+    let check = false
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (j <= board[i].length - 3) {
+                if (board[i][j] != "_" && board[i][j] === board[i][j + 1] && board[i][j] === board[i][j + 2]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+
+            if (j >= 2) {
+                if (board[i][j] != "_" && board[i][j] === board[i][j - 1] && board[i][j] === board[i][j - 2]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+            if (i >= 2) {
+                if (board[i][j] != "_" && board[i][j] === board[i - 1][j] && board[i][j] === board[i - 2][j]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+            if (i <= board.length - 3) {
+                if (board[i][j] != "_" && board[i][j] === board[i+1][j] && board[i][j] === board[i+1][j]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+            if (i >= 2 && j <= board[i].length - 3) {
+                if (board[i][j] != "_" && board[i][j] === board[i - 1][j+1] && board[i][j] === board[i - 2][j+2]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+            if (i >= 2 && j >= 2) {
+                if (board[i][j] != "_" && board[i][j] === board[i - 1][j-1] && board[i][j] === board[i - 2][j-2]) {
+                    sign = board[i][j]
+                    check = true
+                }
+            }
+        }
+        if (check === true) {
+            for (let p of players) {
+                if (p.sign === board[i][j]) {
+                    console.log(p.name, "win");
+                    return true
+                }
+            }
+        }
+        return false
+    }
 }
 
